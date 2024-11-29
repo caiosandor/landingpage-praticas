ce7d895201684bfe87a4f17fa7e892fd69dd7f78
Caio Sandor Jambor Oliveira <146789084+caiosandor@users.noreply.github.com>
2024-11-29 00:19:01 -0300

    Update site.js

--- site.js
+++ site.js
@@ -6,3 +6,38 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('active'); 
     });
 });
+
+
+
+const carousel = document.getElementById("carousel");
+const images = document.querySelectorAll("#carousel img");
+const totalImages = images.length;
+
+let currentIndex = 0; // Índice da imagem atual
+let interval; // Variável para o intervalo de tempo
+
+
+function updateCarousel() {
+    const offset = -currentIndex * 100; // Move o carrossel para a esquerda
+    carousel.style.transform = `translateX(${offset}%)`;
+}
+
+
+function startCarousel() {
+    interval = setInterval(() => {
+        currentIndex = (currentIndex + 1) % totalImages; // Avança para a próxima imagem
+        updateCarousel();
+    }, 2000); 
+}
+
+
+function stopCarousel() {
+    clearInterval(interval);
+}
+
+
+carousel.addEventListener("mouseenter", stopCarousel);
+carousel.addEventListener("mouseleave", startCarousel);
+
+
+startCarousel();
